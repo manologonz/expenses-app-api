d44475802669872f644d0a74bde2cb0370bd1a82
manologonz <manologonz203@gmail.com>
2025-10-09 02:00:00 -0600

    updating dependencies

--- src/api/app.ts
+++ src/api/app.ts
@@ -2,8 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
-import paginate from "express-paginate";
-import {notFound, errorHandler} from "../utils/middlewares";
+import { notFound, errorHandler } from "../utils/middlewares";
 import AllRoutes from "./routes";
 
 // app instantiation
@@ -11,13 +10,12 @@ const app = express();
 
 // middlewares
 if (process.env.NODE_ENV !== "test") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(paginate.middleware(10, 50));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api", AllRoutes);
